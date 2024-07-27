7058fecfb22aa6c20cb44e109f7592a4d7f747b3
jbcax <113384727+jbcax@users.noreply.github.com>
2024-07-26 20:50:29 -0400

    Exercises 3.15-3.18

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -5,13 +5,10 @@ const morgan = require("morgan");
 const cors = require("cors");
 const Person = require("./models/person");
 
-// const password = process.argv[2];
-
+app.use(express.json());
 morgan.token("postData", (request) => {
   return JSON.stringify(request.body);
 });
-
-app.use(express.json());
 app.use(
   morgan(
     ":method :url :status :res[content-length] - :response-time ms :postData"
@@ -20,36 +17,15 @@ app.use(
 app.use(cors());
 app.use(express.static("dist"));
 
-let persons = [
-  {
-    id: "1",
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: "2",
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: "3",
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
 });
 
 app.get("/info", (request, response) => {
-  response.send(`<p>Phonebook has info for ${persons.length} people</p> 
-    <p>${Date()}</p>`);
+  Person.find({}).then((persons) => {
+    response.send(`<p>Phonebook has info for ${persons.length} people</p> 
+     <p>${Date()}</p>`);
+  });
 });
 
 app.get("/api/persons", (request, response) => {
@@ -58,27 +34,40 @@ app.get("/api/persons", (request, response) => {
   });
 });
 
-app.get("/api/persons/:id", (request, response) => {
-  const id = request.params.id;
-  const person = persons.find((person) => person.id === id);
-
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
-  }
+app.get("/api/persons/:id", (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
-app.delete("/api/persons/:id", (request, response) => {
-  const id = request.params.id;
-  persons = persons.filter((person) => person.id !== id);
-
-  response.status(204).end();
+app.delete("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
-const generateId = () => {
-  return String(Math.random() * 999999 + 1);
-};
+app.put("/api/persons/:id", (request, response, next) => {
+  const body = request.body;
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      response.json(updatedPerson);
+    })
+    .catch((error) => next(error));
+});
 
 app.post("/api/persons", (request, response) => {
   const body = request.body;
@@ -98,6 +87,26 @@ app.post("/api/persons", (request, response) => {
     response.json(savedNote);
   });
 });
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
